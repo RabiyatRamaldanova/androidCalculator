39e79a123192fd4debfd2482b9a03cb7acfa4173
AkhundzadaRahid <axundzada@gmail.com>
2020-11-25 17:48:30 +0400

    add calculator

---
+++ app/src/main/java/com/example/calcapp/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.calcapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun onClickSetNumber(view: View) {
+        val textView: TextView = findViewById(R.id.textView)
+        textView.text = view.tag.toString()
+    }
+}
