94264ce7692438798ce44eaafc218a12b28cbb3c
RabiyatRamaldanova <74679051+RabiyatRamaldanova@users.noreply.github.com>
2020-11-25 23:57:23 +0400

    when else

--- app/src/main/java/com/example/calcapp/MainActivity.kt
+++ app/src/main/java/com/example/calcapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.calcapp
 
+import android.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -8,13 +9,27 @@ import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var textView: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportActionBar?.hide()
+        textView = findViewById(R.id.textView)
     }
 
     fun onClickSetNumber(view: View) {
-        val textView: TextView = findViewById(R.id.textView)
+        //val textView: TextView = findViewById(R.id.textView)
         textView.text = view.tag.toString()
+    }
+
+    fun onClickSetSign(view: View) {
+        //if (view.tag == 10){
+            //textView.text = view.tag.toString()
+        //}
+        when(view.tag.toString()){
+            "10" -> textView.text = view.tag.toString()
+            else -> textView.text = ""
+        }
+        //textView.text = view.tag.toString()
     }
 }
