84e919a5a6eed18c1db4235f43184cacf7f133fd
AkhundzadaRahid <axundzada@gmail.com>
2020-11-26 14:24:11 +0400

    add button

--- app/src/main/java/com/example/calcapp/MainActivity.kt
+++ app/src/main/java/com/example/calcapp/MainActivity.kt
@@ -6,30 +6,43 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
+import androidx.core.text.isDigitsOnly
 
 class MainActivity : AppCompatActivity() {
+    private var stringfordisplay = ""
 
     private lateinit var textView: TextView
+    private var dotBool: Boolean = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         supportActionBar?.hide()
         textView = findViewById(R.id.textView)
+
     }
 
     fun onClickSetNumber(view: View) {
-        //val textView: TextView = findViewById(R.id.textView)
-        textView.text = view.tag.toString()
+        stringfordisplay = textView.text.toString()
+        textView.text = stringfordisplay + view.tag.toString()
+        // else {textView.text = view.tag.toString()}
+        if (!textView.text.isEmpty() ) {
+            //textView.text = view.tag.toString()+textView.text
+            if (view.tag.toString() == "16" && !dotBool && textView.text.toString().isDigitsOnly()) {
+                //stringfordisplay = textView.text.toString()
+                textView.text = stringfordisplay + "."
+                dotBool = true
+            }
+        }
     }
 
-    fun onClickSetSign(view: View) {
-        //if (view.tag == 10){
-            //textView.text = view.tag.toString()
-        //}
-        when(view.tag.toString()){
-            "10" -> textView.text = view.tag.toString()
-            else -> textView.text = ""
+        fun onClickSetSign(view: View) {
+            when (view.tag.toString()) {
+                "11" -> textView.text = "/"
+                "12" -> textView.text = "*"
+                "13" -> textView.text = "+"
+                "14" -> textView.text = "-"
+                "15" -> textView.text = "="
+                else -> textView.text = ""
+            }
         }
-        //textView.text = view.tag.toString()
     }
-}
